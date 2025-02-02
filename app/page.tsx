d16e88c3e2ff1008581ace341b48dd2efa6e60a7
junjimoriyama <pero197030@icloud.com>
2025-02-02 15:53:00 +0900

    背景設定

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { ContextPage } from "@/components/ContextPage/ContextPage";
 
 import "./home.scss"
+import ThreeJSExample from "@/components/BackGround/BackGround";
 
 export default function Home() {
   return (
     <div className="home">
     <ContextPage />
+    <ThreeJSExample/>
     </div>
   );
 }
