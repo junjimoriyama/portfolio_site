03286e99df83509427253d9984985a9ae9ea16f8
junjimoriyama <pero197030@icloud.com>
2025-02-03 18:48:30 +0900

    stateにisVisible使用

--- components/ContextPage/Creation/Creation.tsx
+++ components/ContextPage/Creation/Creation.tsx
@@ -71,7 +71,7 @@ export const Creation = () => {
 
   
   items.forEach(({ ref, state }) => {
-    const [ _unused, setIsVisible ] = state;
+    const [ isVisible, setIsVisible ] = state;
     useIntersectionObserver(ref, setIsVisible, { threshold: 0.2 });
   });
 
