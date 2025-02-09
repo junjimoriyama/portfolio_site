49c1b717e882a448385bab559b3a8d63bf51efe8
junjimoriyama <pero197030@icloud.com>
2025-02-09 20:37:52 +0900

    ライトの色暗めに変更

--- components/ContextPage/BackGround/BackGround.tsx
+++ components/ContextPage/BackGround/BackGround.tsx
@@ -123,9 +123,9 @@ export const Background: React.FC = () => {
 
     // X,Y,Z座標それぞれ設定
     for (let i = 0; i < particleCount; i++) {
-      positions[i * 3] = (Math.random() - 0.5) * 10;
-      positions[i * 3 + 1] = (Math.random() - 0.5) * 10;
-      positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+      positions[i * 3] = (Math.random() - 0.5) * 12;
+      positions[i * 3 + 1] = (Math.random() - 0.5) * 12;
+      positions[i * 3 + 2] = (Math.random() - 0.5) * 12;
     }
 
     particles.setAttribute("position", new THREE.BufferAttribute(positions, 3));
