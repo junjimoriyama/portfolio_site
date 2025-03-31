3453c2d364d1e29bdac5bc3624ae3520a75432fc
junjimoriyama <pero197030@icloud.com>
2025-03-31 11:21:09 +0900

    紹介文修正

--- components/ContextPage/Creation/Creation.tsx
+++ components/ContextPage/Creation/Creation.tsx
@@ -136,6 +136,7 @@ export const Creation = () => {
             <div className="creation_item_skills">
               <span>TypeScript</span>
               <span>Next.js</span>
+              <span>React</span>
               <span>RubyOnRails</span>
             </div>
             <div className="creation_item_description">
@@ -240,11 +241,11 @@ export const Creation = () => {
             </div>
             <div className="creation_item_skills">
               <span>TypeScript</span>
-              <span>Vue.js</span>
               <span>Nuxt.js</span>
+              <span>Vue.js</span>
             </div>
             <div className="creation_item_description">
-              TODOリスト管理アプリ
+              TODOリスト管理アプリ<span>(個人開発)</span>
             </div>
             <div className="creation_item_git">
               <Link
@@ -293,9 +294,10 @@ export const Creation = () => {
             <div className="creation_item_skills">
               <span>TypeScript</span>
               <span>Next.js</span>
+              <span>React</span>
             </div>
             <div className="creation_item_description">
-              仮想ショッピングページ
+              仮想ショッピングページ<span>(個人開発)</span>
             </div>
             <div className="creation_item_git">
               <Link
@@ -401,113 +403,3 @@ export const Creation = () => {
     </div>
   );
 };
-
-// "use client";
-
-// // next
-// import Image from "next/image";
-// import Link from "next/link";
-// // react
-// import { useSwitchContext } from "@/components/context/switchProvider";
-// import { useEffect, useRef, useState } from "react";
-// // functions
-// import useIntersectionObserver from "@/functions/functions";
-// // svg
-// import { PcSvg } from "@/assets/svg/PcSvg";
-// // style
-// import "./Creation.scss";
-
-// export const Creation = () => {
-//   const { isSwitchOn } = useSwitchContext();
-
-//   // 初めはImageタグの追加クラスは空にする
-//   const [imageState, setImageState] = useState("");
-
-//   useEffect(() => {
-//     // もしスイッチがONであれば
-//     if (isSwitchOn) {
-//       setImageState("isOn");
-//       // もしスイッチがONであればOFFを実行できる
-//     } else if (imageState === "isOn") {
-//       setImageState("isOff");
-//     }
-//   }, [isSwitchOn, imageState]);
-
-//   const items = [
-//     {
-//       ref: useRef<HTMLDivElement | null>(null),
-//       state: useState(false),
-//       link: "https://stuttering-job-web.vercel.app/",
-//       image: "/images/stuttering.png",
-//       title: "吃音と仕事DATABASE",
-//       description: "働く吃音ある方の体験談集",
-//       tags: ["SITE", "APPLICATION", "レスポンシブ"]
-//     },
-//     {
-//       ref: useRef<HTMLDivElement | null>(null),
-//       state: useState(false),
-//       link: "https://junji-moriyama.com/",
-//       image: "/images/photosite.png",
-//       title: "PHOTO SITE",
-//       description: "個人で撮影した写真ギャラリーサイト",
-//       tags: ["SITE", "レスポンシブ"]
-//     },
-//     {
-//       ref: useRef<HTMLDivElement | null>(null),
-//       state: useState(false),
-//       link: "https://task-management-lake-nine.vercel.app/",
-//       image:  "/images/todo.png",
-//       title: "TODO APP",
-//       description: "TODOリスト管理アプリ",
-//       tags: ["APPLICATION"]
-//     },
-//     {
-//       ref: useRef<HTMLDivElement | null>(null),
-//       state: useState(false),
-//       link: "https://shopping-app-next-ten.vercel.app/",
-//       image:  "/images/superMarket.png",
-//       title: "SUPER MARKET",
-//       description: "仮想ショッピングサイト",
-//       tags: ["APPLICATION"]
-//     },
-//   ];
-
-//   useEffect(() => {
-//     items.forEach(({ ref, state }) => {
-//       const [, setIsVisible] = state;
-//       if (ref.current) {
-//         useIntersectionObserver(ref, setIsVisible, { threshold: 0.2 });
-//       }
-//     });
-//   }, [items]);
-
-//   return (
-//     <div className="creation">
-//     {items.map(({ ref, state, link, image, title, description, tags }, index) => {
-//       const [isVisible] = state;
-//       return (
-//         <div
-//           key={index}
-//           className={`creation_item ${isVisible ? "isVisible" : ""}`}
-//           ref={ref}
-//         >
-//           <PcSvg />
-//           <div className={`creation_item_image ${isSwitchOn ? "isSwitchOn" : ""}`}>
-//             <Link href={link} target="_blank" rel="noopener noreferrer">
-//               <Image className={`thumbnail_img ${imageState}`} src={image} alt={`${title}画像`} width={310} height={173} />
-//             </Link>
-//           </div>
-//           <p className={`creation_item_visit ${isSwitchOn ? "isVisible" : ""}`}>VISIT</p>
-//           <div className="creation_item_about">
-//             <div className="creation_item_title">{title}</div>
-//             <div className="creation_item_type">
-//               {tags.map((tag, idx) => <span key={idx}>{tag}</span>)}
-//             </div>
-//             <div className="creation_item_description">{description}</div>
-//           </div>
-//         </div>
-//       );
-//     })}
-//   </div>
-//   );
-// };
--- components/ContextPage/Profile/Profile.tsx
+++ components/ContextPage/Profile/Profile.tsx
@@ -1,65 +1,65 @@
 "use client";
 
 // next
-import Link from 'next/link'
+import Link from "next/link";
 // functions
 import useIntersectionObserver from "@/functions/functions";
 // svg
-import { GitIcon } from '@/assets/svg/GitIcon'
+import { GitIcon } from "@/assets/svg/GitIcon";
 // style
-import './Profile.scss'
-import { useRef, useState } from 'react';
-import { useSwitchContext } from '@/components/context/switchProvider';
+import "./Profile.scss";
+import { useRef, useState } from "react";
+import { useSwitchContext } from "@/components/context/switchProvider";
 
 export const Profile = () => {
-
   const { isSwitchOn } = useSwitchContext();
 
   const refs = {
     myName: useRef(null),
     description: useRef(null),
-  }
+  };
 
-  const [ isMyNameVisible, setIsMyNameVisible  ] = useState(false);
-  const [ isDescriptionVisible, setIsDescriptionVisible ] = useState(false);
+  const [isMyNameVisible, setIsMyNameVisible] = useState(false);
+  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
 
   useIntersectionObserver(refs.myName, setIsMyNameVisible, {
-    threshold : 0.5
-  })
+    threshold: 0.5,
+  });
   useIntersectionObserver(refs.description, setIsDescriptionVisible, {
-    threshold: 0.5
-  })
-
+    threshold: 0.5,
+  });
 
   return (
     <div className={`profile ${isSwitchOn ? "isSwitchOn" : ""}`}>
       <div className="profile_wrap">
-      <div 
-      className={`profile_myName ${isMyNameVisible ? "isVisible" : ""}`}
-      ref={refs.myName}
-      >
-        森山順司
-        <span className='git_link'>
-        <Link 
-        href="https://github.com/junjimoriyama" 
-        target="_blank"
-        rel="noopener noreferrer"
+        <div
+          className={`profile_myName ${isMyNameVisible ? "isVisible" : ""}`}
+          ref={refs.myName}
         >
-          <GitIcon />
-        </Link>
-        </span>
-      </div>
-      
-      <div 
-      className={`profile_description ${isDescriptionVisible ? "isVisible" : ""}`}
-      ref={refs.description}
-      >
-        <p>フロントエンドに惹かれて3年。</p>
-        <p>シンプルでストレスのない設計を好み、アニメーションを加えた少し遊び心のあるUI/UXが好きです。</p>
+          森山順司
+          <span className="git_link">
+            <Link
+              href="https://github.com/junjimoriyama"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GitIcon />
+            </Link>
+          </span>
         </div>
 
+        <div
+          className={`profile_description ${
+            isDescriptionVisible ? "isVisible" : ""
+          }`}
+          ref={refs.description}
+        >
+          <p>フロントエンドに惹かれて3年。</p>
+          <p>
+          シンプルでストレスのない設計を好み、アニメーションを加えた遊び心のあるUI/UXを目指し、デザインから実装まで行っています。
+          </p>
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
