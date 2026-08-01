461c11ca817d2e0b21c1a23b8c7ea3dde2a8c9c2
junjimoriyama <pero197030@icloud.com>
2026-08-01 12:19:17 +0900

    ESLintエラー無視

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // ビルド時にESLintのエラーを無視する
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
